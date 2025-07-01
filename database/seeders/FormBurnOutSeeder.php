7c41dc8c0d4bd00ff13b763b2af22064b567b44b
wheelkorner <wheelkorner@gmail.com>
2025-07-01 08:03:19 -0300

    alteracoes edilaine

--- database/seeders/FormBurnOutSeeder.php
+++ database/seeders/FormBurnOutSeeder.php
@@ -19,7 +19,7 @@ class FormBurnOutSeeder extends Seeder
                 'nome' => 'Burnout 99',
                 'label' => 'BO99',
                 'descricao' => '<p>Questionário de Riscos Psicossociais (Base NR-1 e MBI Adaptado)</p><p>Avaliação Individual</p><p>Este relatório apresenta a análise das dimensões psicossociais avaliadas conforme os parâmetros da <b>NR-1</b> e do inventário <b>MBI</b> adaptado. </p><p>As faixas são classificadas como Baixa, Moderada ou Alta, com base nas pontuações obtidas.</p>',
-                'instrucoes' => '<p>Responda cada uma das afirmações abaixo de acordo com a frequência com que você se sente assim no ambiente de trabalho. </p><p>Use a escala de 1 a 5, onde:</p><ul><li>1 - Uma vez por mês ou menos</li><li>2 - Algumas vezes por mês</li><li>3 - Uma vez por semana</li><li>4 - Algumas vezes por semana</li><li>5 - Todos os dias</li></ul>',
+                'instrucoes' => '<p>Responda com toda a sinceridade, sabendo que o sigilo da sua identidade é absoluto e garantido por nós, e que seu feedback é indispensável para construir um ambiente de trabalho mais sustentável e positivo para todos. Você está em um espaço 100% seguro, e cada resposta é valiosa para elevar sua qualidade de vida. </p><p>Use a escala de 1 a 5, onde:</p><ul><li>1 - Uma vez por mês ou menos</li><li>2 - Algumas vezes por mês</li><li>3 - Uma vez por semana</li><li>4 - Algumas vezes por semana</li><li>5 - Todos os dias</li></ul>',
                 'score_ini' => 1,
                 'score_fim' => 5,
                 'calculo_id' => 1,
